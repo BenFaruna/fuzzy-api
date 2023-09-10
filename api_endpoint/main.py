29ee216a6c7819978778e6a4380e0a43b974fc60
BenFaruna <benjaminfaruna@gmail.com>
2023-09-10 23:47:22 +0100

    Completed CRUD endpoints for person model and added parameter validation for routing

--- api_endpoint/main.py
+++ api_endpoint/main.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 
 from flask import jsonify, request
 
-from api_endpoint.utils import create_app, create_database_session
+from api_endpoint.utils import (
+    create_app, create_database_session, validate_url_parameter
+    )
 from api_endpoint.models.person import Person
 
 load_dotenv()
@@ -14,9 +16,17 @@ app = create_app()
 engine, session = create_database_session()
 
 
-@app.route("/api/user/<string:name>", methods=["GET"], strict_slashes=False)
+@app.route("/api/<string:name>", methods=["GET"], strict_slashes=False)
 def get_user(name):
     # Todo: validation of parameter data type
+    try:
+        if not validate_url_parameter(name):
+            raise ValueError(
+                "/api/<name> - all characters in <name> should be alphabets"
+                )
+    except ValueError as e:
+        return jsonify({"Error": str(e)}), 400
+
 
     person = session.query(Person).filter(Person.name == name).first()
 
@@ -25,7 +35,7 @@ def get_user(name):
     else:
         return jsonify({"Error": f"{name} not found"}), 404
 
-@app.route("/api/user", methods=["POST"], strict_slashes=False)
+@app.route("/api", methods=["POST"], strict_slashes=False)
 def create_user():
     body = request.get_json()
 
@@ -39,8 +49,16 @@ def create_user():
         return jsonify({"Error": str(e)}), 500
 
 
-@app.route("/api/user/<string:name>", methods=["PUT"])
+@app.route("/api/<string:name>", methods=["PUT"], strict_slashes=False)
 def update_name(name):
+    try:
+        if not validate_url_parameter(name):
+            raise ValueError(
+                "/api/<name> - all characters in <name> should be alphabets"
+                )
+    except ValueError as e:
+        return jsonify({"Error": str(e)}), 400
+
     body = request.get_json()
 
     user = session.query(Person).filter(Person.name == name).first()
@@ -55,9 +73,35 @@ def update_name(name):
         else:
             return jsonify({"Error": "new name not supplied"}), 400
     except Exception as e:
+        session.rollback()
         return jsonify({"Error": str(e)}), 500
 
     return jsonify({"Success": "Name updated!"})
+
+
+@app.route("/api/<string:name>", methods=["DELETE"], strict_slashes=False)
+def delete_user(name):
+    try:
+        if not validate_url_parameter(name):
+            raise ValueError(
+                "/api/<name> - all characters in <name> should be alphabets"
+                )
+    except ValueError as e:
+        return jsonify({"Error": str(e)}), 400
+
+    user = session.query(Person).filter(Person.name == name).first()
+
+    if not user:
+        return jsonify({"Error": f"{name} cannot be found"}), 404
+
+    try:
+        session.delete(user)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        return jsonify({"Error": str(e)}), 500
+
+    return jsonify({"Success": f"{name} deleted!"})
 
 
 if __name__ == "__main__":
--- api_endpoint/utils.py
+++ api_endpoint/utils.py
@@ -1,15 +1,17 @@
 import os
 
+from typing import Union
+
 from flask import Flask
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     return app
 
-def create_database_session():
+def create_database_session() -> Union[Engine, Session]:
     DB_NAME = os.getenv("DB_NAME")
     DB_HOST = os.getenv("DB_HOST")
     DB_USER = os.getenv("DB_USER")
@@ -22,3 +24,11 @@ def create_database_session():
     Session = sessionmaker(bind=engine)
     session = Session()
     return engine, session
+
+
+def validate_url_parameter(param: str) -> bool:
+    """helper function to help validate the data type of dynamic parameters"""
+    if param.isalpha():
+        return True
+    else:
+        return False
